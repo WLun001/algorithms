913310aea9496cde480e352b817560e39cfa408b
Wei Lun <weilun_95@hotmail.com>
2018-05-05 15:23:17 +0800

    completed contains duplicate

--- src/test/testArrayAndString.kt
+++ src/test/testArrayAndString.kt
@@ -1,5 +1,6 @@
 package test
 
+import main.ArrayAndStrings.containsDuplicate
 import main.ArrayAndStrings.removeDuplicates
 import main.ArrayAndStrings.rotate
 import org.junit.jupiter.api.Test
@@ -22,6 +23,17 @@ class TestArrayAndString {
         val arr2 = intArrayOf(-1, -100, 3, 99)
         assertTrue(Arrays.equals(intArrayOf(5, 6, 7, 1, 2, 3, 4), rotate(arr, 3)))
         assertTrue(Arrays.equals(intArrayOf(3, 99, -1, -100), rotate(arr2, 2)))
+    }
+
+    @Test
+    fun testContainsDuplicate() {
+        val arr = intArrayOf(1, 2, 3, 1)
+        val arr2 = intArrayOf(1, 2, 3, 4)
+        val arr3 = intArrayOf(1, 1, 1, 3, 3, 4, 3, 2, 4, 2)
+        assertEquals(true, containsDuplicate(arr))
+        assertEquals(false, containsDuplicate(arr2))
+        assertEquals(true, containsDuplicate(arr3))
+
     }
 }
 
