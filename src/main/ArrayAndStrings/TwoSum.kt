5e44e6b94cb014e2b0052a1411378496b20d6793
Wei Lun <weilun_95@hotmail.com>
2018-05-05 22:29:31 +0800

    added two sum and reformat

---
+++ src/main/ArrayAndStrings/TwoSum.kt
@@ -0,0 +1,24 @@
+package ArrayAndStrings
+
+/*
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].*/
+
+fun twoSum(nums: IntArray, target: Int): IntArray {
+    val arr = ArrayList<Int>()
+    nums.indices.forEach { i ->
+        if (arr.size >= 2) return@forEach
+        for (j in nums.indices) {
+            if (i != j && nums[i] + nums[j] == target) with(arr) { add(i); add(j) }
+        }
+    }
+    return arr.toIntArray()
+}
--- src/test/testArrayAndString.kt
+++ src/test/testArrayAndString.kt
@@ -2,6 +2,7 @@ package test
 
 import ArrayAndStrings.moveZeroes
 import ArrayAndStrings.plusOne
+import ArrayAndStrings.twoSum
 import main.ArrayAndStrings.*
 import org.junit.jupiter.api.Test
 import java.util.*
@@ -37,8 +38,8 @@ class TestArrayAndString {
 
     @Test
     fun testSingleNumber() {
-        val arr = intArrayOf(2,2,1)
-        val arr2 = intArrayOf(4,1,2,1,2)
+        val arr = intArrayOf(2, 2, 1)
+        val arr2 = intArrayOf(4, 1, 2, 1, 2)
         assertEquals(1, singleNumber(arr))
         assertEquals(4, singleNumber(arr2))
     }
@@ -46,26 +47,33 @@ class TestArrayAndString {
     @Test
     fun testIntersection() {
         val arr = intArrayOf(1, 2, 2, 1)
-        val arr2 = intArrayOf(2,2,3)
+        val arr2 = intArrayOf(2, 2, 3)
         assertTrue(Arrays.equals(intArrayOf(2), intersect(arr, arr2)))
     }
 
     @Test
     fun testPlusOne() {
-        val arr = intArrayOf(1,2,3)
-        val arr2 = intArrayOf(1,3,9)
-        val arr3 = intArrayOf(4,3,2,1)
-        assertTrue(Arrays.equals(intArrayOf(1,2,4), plusOne(arr)))
-        assertTrue(Arrays.equals(intArrayOf(1,4,0), plusOne(arr2)))
-        assertTrue(Arrays.equals(intArrayOf(4,3,2,2), plusOne(arr3)))
+        val arr = intArrayOf(1, 2, 3)
+        val arr2 = intArrayOf(1, 3, 9)
+        val arr3 = intArrayOf(4, 3, 2, 1)
+        assertTrue(Arrays.equals(intArrayOf(1, 2, 4), plusOne(arr)))
+        assertTrue(Arrays.equals(intArrayOf(1, 4, 0), plusOne(arr2)))
+        assertTrue(Arrays.equals(intArrayOf(4, 3, 2, 2), plusOne(arr3)))
     }
 
     @Test
     fun testMoveZeroes() {
-        val arr = intArrayOf(0,1,0,3,12)
-        val arr2 = intArrayOf(1,20,0,200,0,1,20,2)
-        assertTrue(Arrays.equals(intArrayOf(1,3,12), moveZeroes(arr)))
-        assertTrue(Arrays.equals(intArrayOf(1,1,2,20,20,200), moveZeroes(arr2)))
+        val arr = intArrayOf(0, 1, 0, 3, 12)
+        val arr2 = intArrayOf(1, 20, 0, 200, 0, 1, 20, 2)
+        assertTrue(Arrays.equals(intArrayOf(1, 3, 12), moveZeroes(arr)))
+        assertTrue(Arrays.equals(intArrayOf(1, 1, 2, 20, 20, 200), moveZeroes(arr2)))
+    }
+
+    @Test
+    fun testTwoSum() {
+        val arr = intArrayOf(2, 7, 11, 15)
+        assertTrue(Arrays.equals(intArrayOf(0, 1), twoSum(arr, 9)))
+        assertTrue(Arrays.equals(intArrayOf(1, 3), twoSum(arr, 22)))
     }
 }
 
