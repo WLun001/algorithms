8aafdc39e4dbd86e7227f3b108febec26563d531
Wei Lun <weilun_95@hotmail.com>
2018-06-10 16:24:28 +0800

    added practice section

---
+++ src/main/practice/DirectorySize.kt
@@ -0,0 +1,25 @@
+package practice
+
+import java.io.File
+import java.util.*
+
+fun main(args: Array<String>) {
+    print("Enter a directory or a file: ")
+    val input = Scanner(System.`in`)
+    val directory = input.nextLine()
+    // Display the size
+    println(getSize(File(directory)).toString() + " bytes")
+}
+
+fun getSize(file: File): Long {
+    var fileSize: Long = 0
+    if (file.isDirectory) {
+        val files = file.listFiles()
+        for (f in files) {
+            fileSize += getSize(f)
+        }
+    }
+    else fileSize += file.length()
+    return fileSize
+}
+
