2a182924c87d9f41ddec10d943405a342f640a7e
Wei Lun <weilun_95@hotmail.com>
2018-07-01 14:36:22 +0800

    completed eva expression

--- src/main/practice/EvaluateExpression.kt
+++ src/main/practice/EvaluateExpression.kt
@@ -3,17 +3,17 @@ package practice
 import java.util.*
 
 fun main(args: Array<String>) {
-    removeWhiteSpace("(1 + 2) / 20").forEach { println(message = it) }
+    println(evaluateExpression("(1+2) * 3"))
+    println(evaluateExpression("(1+2) / 3"))
+    println(evaluateExpression("3 * ( 1 + 2 + 3)"))
 }
 
 data class Stacks(val operandStack: Stack<Int>, val operatorStack: Stack<Char>)
 
 fun evaluateExpression(expression: String): Int {
-
     val tokens = removeWhiteSpace(expression)
     val (operandStack, operatorStack) = scanTokenIntoStacks(tokens)
-    
-    return 0
+    return clearStacks(operandStack, operatorStack)
 }
 
 fun scanTokenIntoStacks(tokens: CharArray): Stacks {
@@ -21,30 +21,46 @@ fun scanTokenIntoStacks(tokens: CharArray): Stacks {
     val operatorStack = Stack<Char>()
     for (token in tokens) {
         if (token == '+' || token == '-') {
-            while (!operatorStack.empty() &&
+            while (!operatorStack.isEmpty() &&
                     (operatorStack.peek() == '+' ||
                             operatorStack.peek() == '-' ||
                             operatorStack.peek() == '*' ||
                             operatorStack.peek() == '/')) processAnOperator(operandStack, operatorStack)
             operatorStack.push(token)
         } else if (token == '*' || token == '/') {
-            while (!operatorStack.empty() &&
+            while (!operatorStack.isEmpty() &&
                     (operatorStack.peek() == '*' || operatorStack.peek() == '/'))
                 processAnOperator(operandStack, operatorStack)
             operatorStack.push(token)
         } else if (token == '(') operatorStack.push(token)
         else if (token == ')') {
             while (operatorStack.peek() != '(') processAnOperator(operandStack, operatorStack)
-            operatorStack.push(token)
-        } else operandStack.push(token.toInt())
+            operatorStack.pop()
+        } else {
+            operandStack.push(token.toString().toInt()) // if token.toInt() will return its ASCII code
+        }
     }
-
     return Stacks(operandStack, operatorStack)
+}
 
+fun clearStacks(operandStack: Stack<Int>, operatorStack: Stack<Char>): Int {
+    while (true) {
+        if (operatorStack.empty()) break
+        else processAnOperator(operandStack, operatorStack)
+    }
+    return operandStack.pop()
 }
 
 fun processAnOperator(operandStack: Stack<Int>, operatorStack: Stack<Char>) {
-
+    val operator = operatorStack.pop()
+    val operand1 = operandStack.pop()
+    val operand2 = operandStack.pop()
+    when (operator) {
+        '+' -> operandStack.push(operand2 + operand1)
+        '-' -> operandStack.push(operand2 - operand1)
+        '*' -> operandStack.push(operand2 * operand1)
+        '/' -> operandStack.push(operand2 / operand1)
+    }
 }
 
 fun removeWhiteSpace(expression: String): CharArray {
