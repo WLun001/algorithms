c0c9eb5bc6dcdc6b48d05ec7eb6455c247ccdcf3
Wei Lun <weilun_95@hotmail.com>
2018-07-01 15:07:00 +0800

    added Kdoc

--- src/main/practice/EvaluateExpression.kt
+++ src/main/practice/EvaluateExpression.kt
@@ -10,12 +10,32 @@ fun main(args: Array<String>) {
 
 data class Stacks(val operandStack: Stack<Int>, val operatorStack: Stack<Char>)
 
+
+/**
+ * evaluate expression in 2 phases.
+ * - Phase 1: [scan token][scanTokenIntoStacks].
+ * - Phase 2: [clear stack][clearStacks].
+ *
+ * @param expression maths expression
+ * @return the answer of the maths expression
+ */
 fun evaluateExpression(expression: String): Int {
     val tokens = removeWhiteSpace(expression)
     val (operandStack, operatorStack) = scanTokenIntoStacks(tokens)
     return clearStacks(operandStack, operatorStack)
 }
 
+/**
+ * Phase 1: Scanning the expression
+ * - 1.1 If the extracted item is an operand, push it to operandStack.
+ * - 1.2 If the extracted item is + or - operator, process all the operators at the top of operatorStack and push the extracted operator to operatorStack.
+ * - 1.3 If the extracted item is * or / operator, process all the * or / operators at the top of operatorStack and push the extracted operator to operatorStack.
+ * - 1.4 If the extracted item is ( symbol, push it to operatorStack.
+ * - 1.5 If the extracted item is ) symbol, repeatedly process the operators from the top of operatorStack until seeing the ( symbol on the stack.
+ *
+ * @param tokens expression in CharArray
+ * @return [Stacks.operandStack] and [Stacks.operatorStack]
+ */
 fun scanTokenIntoStacks(tokens: CharArray): Stacks {
     val operandStack = Stack<Int>()
     val operatorStack = Stack<Char>()
@@ -43,6 +63,13 @@ fun scanTokenIntoStacks(tokens: CharArray): Stacks {
     return Stacks(operandStack, operatorStack)
 }
 
+/**
+ * Phase 2: Clearing the stacks
+ * - Repeatedly process the operators from the top of operatorStack until operatorStack is empty
+ * @param operandStack
+ * @param operatorStack
+ * @return final answer of the expression
+ */
 fun clearStacks(operandStack: Stack<Int>, operatorStack: Stack<Char>): Int {
     while (true) {
         if (operatorStack.empty()) break
