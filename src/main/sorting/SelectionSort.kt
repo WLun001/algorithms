211efca489f2ed82f38db1a6eaa2b15940899b51
Wei Lun <weilun_95@hotmail.com>
2018-08-20 23:39:20 +0800

    added selectionSort

--- src/main/sorting/SelectionSort.kt
+++ src/main/sorting/SelectionSort.kt
@@ -1,2 +1,25 @@
 package sorting
 
+fun main(args: Array<String>) {
+    val list = intArrayOf(2, 3, 2, 5, 6, 1, -2, 3, 14, 12)
+    selectionSort(list).forEach { println(it) }
+}
+
+fun selectionSort(arr: IntArray): IntArray {
+    val arrList = arr.toMutableList()
+    for (i in 0 until arrList.size) {
+        var currentMin = arrList[i]
+        var currentMinIndex = i
+        for (j in i + 1 until arrList.size) {
+            if (arrList[j] < currentMin) {
+                currentMin = arrList[j]
+                currentMinIndex = j
+            }
+        }
+        if (currentMinIndex != i) {
+            arrList[currentMinIndex] = arrList[i]
+            arrList[i] = currentMin
+        }
+    }
+    return arrList.toIntArray()
+}
