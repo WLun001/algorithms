bf0dc189f7325fb979159b7d1ea035736cae21cc
Wei Lun <weilun_95@hotmail.com>
2018-08-20 23:19:31 +0800

    added bubblesort

---
+++ src/main/sorting/BubbleSort.kt
@@ -0,0 +1,26 @@
+package sorting
+
+fun main(args: Array<String>) {
+    val list = intArrayOf(2, 3, 2, 5, 6, 1, -2, 3, 14, 12)
+    bubbleSort(list).forEach { println(it) }
+}
+
+fun bubbleSort(arr: IntArray): IntArray {
+    val arrList = arr.toMutableList()
+    val iterable = arrList.size
+    var i = 1
+    var needRepeat = true
+    while (i < iterable && needRepeat) {
+        needRepeat = false
+        for (j in 0 until iterable - i) {
+            if (arrList[j] > arrList[j + 1]) {
+                val temp = arrList[j]
+                arrList[j] = arrList[j + 1]
+                arrList[j + 1] = temp
+                needRepeat = true
+            }
+        }
+        i++
+    }
+    return arrList.toIntArray()
+}
