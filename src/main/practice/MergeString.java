7c3e56228c6152d0fcc9cef03592a839f8edbfca
Wei Lun <weilun_95@hotmail.com>
2019-01-04 12:37:33 +0800

    Added mergeString

---
+++ src/main/practice/MergeString.java
@@ -0,0 +1,33 @@
+package practice;
+
+/**
+ * Merge data from two sources connected to a network access point to create a new data packet
+ * Merge strings a and b, and return a single merged string.
+ * A merge operation on tow strings is described as follows:
+ * - Append alternating characters from a and b.
+ * - Once all of the characters in one of the strings have been merged, append the remaining characters.
+ *
+ * Example:
+ *  "abc" + "stuvwx" will produce "asbtcuvwx"
+ */
+public class MergeString {
+
+    static String mergeStrings(String a, String b) {
+        StringBuilder builder = new StringBuilder();
+        int index = 0;
+        char[] charA = a.toCharArray();
+        char[] charB = b.toCharArray();
+        for (int i = 0; i < charA.length; i++) {
+            index = i;
+            builder.append(charA[i]);
+            builder.append(charB[i]);
+        }
+        for (int i = index + 1; i < charB.length; i++) {
+            builder.append(charB[i]);
+        }
+        return builder.toString();
+    }
+    public static void main(String[] args) {
+        System.out.println(mergeStrings("abc", "stuvwx"));
+    }
+}
